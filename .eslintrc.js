d0e150a1a868aa28262047f37f6513a396fe09a0
Sjors van Holst <sjorsvanholst@gmail.com>
2022-03-25 14:45:01 +0100

    :poop: New version!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    parser: 'babel-eslint'
   },
   rules: {
-    "@typescript-eslint/no-non-null-assertion": "off",
-
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
