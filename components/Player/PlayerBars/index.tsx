fcb633d8d5b1106be687977c462bfb90da84abf6
Sjors van Holst <sjorsvanholst@gmail.com>
2023-07-09 20:41:05 +0200

    ✨ Added bars

--- components/Player/PlayerBars/index.tsx
+++ components/Player/PlayerBars/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { Player, SlurpBar } from "@/types";
+import { useEffect, useState } from "react";
 import React from "react";
 import {
   BarChart,
@@ -13,52 +14,11 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+type PlayerBarsProps = {
+  bars: SlurpBar[];
+};
 
-export default function PlayerBars() {
+export default function PlayerBars({ bars }: PlayerBarsProps) {
   const [height, setHeight] = useState(0);
 
   window.addEventListener("resize", () => {
@@ -71,14 +31,14 @@ export default function PlayerBars() {
 
   return (
     <ResponsiveContainer width="100%" height={height / 2}>
-      <BarChart data={data}>
+      <BarChart data={bars}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="player_username" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-        <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+        <Bar dataKey="units_consumed" stackId="a" fill="#8884d8" />
+        <Bar dataKey="units_consume" stackId="a" fill="#82ca9d" />
       </BarChart>
     </ResponsiveContainer>
   );
